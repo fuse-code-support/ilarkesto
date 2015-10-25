d23e92f8a4575e0b3847dc853bfa2701e0df67ca
Witoslaw Koczewski <wi@koczewski.de>
2015-10-25 12:25:48 +0100

    gwt table column sort toggle icon

--- src/main/java/ilarkesto/gwt/client/desktop/AObjectTableWithGroups.java
+++ src/main/java/ilarkesto/gwt/client/desktop/AObjectTableWithGroups.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -123,7 +123,8 @@ public abstract class AObjectTableWithGroups<O, G> implements IsWidget, Updatabl
 				boolean customSortingEnabled = isCustomSortingEnabled();
 
 				Widget titleWidget = Widgets.textFieldlabel(columnTitle, false);
-				if (titleWidget != null && customSortingEnabled) titleWidget.addStyleName("columnTitleWithSortToggle");
+				if (titleWidget != null && customSortingEnabled && isShowColumnSortingToggleIcon())
+					titleWidget.addStyleName("columnTitleWithSortToggle");
 
 				if (titleWidget != null && (sortColumnIndex == column.index)) {
 					// Sortierte Spalte hervorheben
@@ -172,6 +173,10 @@ public abstract class AObjectTableWithGroups<O, G> implements IsWidget, Updatabl
 			}
 		}
 
+	}
+
+	protected boolean isShowColumnSortingToggleIcon() {
+		return false;
 	}
 
 	public void updateFootCells() {
