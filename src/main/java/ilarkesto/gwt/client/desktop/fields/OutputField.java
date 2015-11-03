b034a1a54d4ed7cf307fa378330e251c71fd9b81
Witoslaw Koczewski <wi@koczewski.de>
2015-11-03 16:34:09 +0100

    gwt output field

--- src/main/java/ilarkesto/gwt/client/desktop/fields/OutputField.java
+++ src/main/java/ilarkesto/gwt/client/desktop/fields/OutputField.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -25,11 +25,21 @@ public class OutputField extends AField {
 	private Object value;
 	private String suffix;
 	private String href;
+	private boolean mandatory = false;
 
 	public OutputField(String label, Object value) {
 		super();
 		this.label = label;
 		this.value = value;
+	}
+
+	public boolean isMandatory() {
+		return mandatory;
+	}
+
+	public OutputField setMandatory(boolean mandatory) {
+		this.mandatory = mandatory;
+		return this;
 	}
 
 	public OutputField setValue(Object value) {
