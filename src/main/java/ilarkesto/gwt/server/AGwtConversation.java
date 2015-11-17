21a32b224ef5e5a5b116c766ceb0f061f86fb4be
Witoslaw Koczewski <wi@koczewski.de>
2015-11-17 18:37:43 +0100

    gwt

--- src/main/java/ilarkesto/gwt/server/AGwtConversation.java
+++ src/main/java/ilarkesto/gwt/server/AGwtConversation.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -176,6 +176,12 @@ public abstract class AGwtConversation<S extends AWebSession> implements ClientD
 	public void deleteFromClient(String entityId) {
 		if (entityId == null) return;
 		getNextData().addDeletedEntity(entityId);
+		for (Entity entity : remoteEntityModificationTimes.keySet()) {
+			if (entity.getId().equals(entityId)) {
+				remoteEntityModificationTimes.remove(entity);
+				break;
+			}
+		}
 	}
 
 	public void sendToClientIfTracking(Entity entity) {
@@ -193,8 +199,8 @@ public abstract class AGwtConversation<S extends AWebSession> implements ClientD
 
 		if (timeLocal.equals(timeRemote)) {
 			if (log.isDebugEnabled())
-				log.debug("RemotEntity entity already up to date:", toString(entity), "for", this, "->", timeLocal,
-					"/", timeRemote);
+				log.debug("Remote entity already up to date:", toString(entity), "for", this, "->", timeLocal, "/",
+					timeRemote);
 			return;
 		}
 
