f5115ab7e7c508a1e7189dbbd70b453fb90454cc
Witoslaw Koczewski <wi@koczewski.de>
2015-11-22 10:41:07 +0100

    gwt

--- src/main/java/ilarkesto/gwt/client/desktop/ActivityParameters.java
+++ src/main/java/ilarkesto/gwt/client/desktop/ActivityParameters.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -115,7 +115,7 @@ public class ActivityParameters implements Serializable, IsSerializable {
 
 	public ActivityParameters put(String name, Object value) {
 		if (value == null) return put(name, null);
-		if (value instanceof AEntity) return put(name, ((AEntity) value).getId());
+		if (value instanceof Entity) return put(name, ((Entity) value).getId());
 		return put(name, value.toString());
 	}
 
