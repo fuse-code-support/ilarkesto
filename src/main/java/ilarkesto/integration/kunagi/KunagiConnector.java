b42520d1dfcff89493aba0d8f6a44ef79eed4797
Witoslaw Koczewski <wi@koczewski.de>
2016-01-21 14:29:08 +0100

    kunagi connector logging

--- src/main/java/ilarkesto/integration/kunagi/KunagiConnector.java
+++ src/main/java/ilarkesto/integration/kunagi/KunagiConnector.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -56,7 +56,7 @@ public class KunagiConnector {
 		data.put("spamPreventionCode", "no-spam");
 		String ret = httpDownloader.post(url, data, IO.UTF_8);
 		if (ret.contains("Submitting issue failed") || ret.contains("Submitting your feedback failed")) {
-			log.error("Submitting issue failed:", ret, "\n", name, email, subject, text, additionalInfo);
+			log.error("Submitting issue failed:", ret, "\n", name, email, subject, text);
 			throw new RuntimeException(ret);
 		}
 		log.info("Issue submitted:", ret);
