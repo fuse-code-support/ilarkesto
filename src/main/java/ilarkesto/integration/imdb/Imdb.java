51beafbb3dcd307e8f233d854fc576c03df3bbf3
Witoslaw Koczewski <wi@koczewski.de>
2016-01-26 15:33:41 +0100

    imdb

--- src/main/java/ilarkesto/integration/imdb/Imdb.java
+++ src/main/java/ilarkesto/integration/imdb/Imdb.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -178,12 +178,12 @@ public class Imdb {
 	private static String parseCoverId(String html) throws ParseException {
 		HtmlPage page = new HtmlParser().parse(html);
 
-		HtmlTag container = page.getTagByStyleClass("poster");
+		HtmlTag container = null; // page.getTagByStyleClass("poster");
 		if (container == null) container = page.getTagById("img_primary");
 
 		if (container == null) return null;
 
-		HtmlTag img = container.getTagByName("img");
+		HtmlTag img = container.isName("img") ? container : container.getTagByName("img");
 		if (img == null) return null;
 
 		String url = img.getAttribute("src");
