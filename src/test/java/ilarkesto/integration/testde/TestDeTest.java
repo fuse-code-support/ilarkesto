76a2bb9c71c2895d47eb4621810e91b5edd92090
Witoslaw Koczewski <wi@koczewski.de>
2016-02-03 10:47:43 +0100

    test.de

--- src/test/java/ilarkesto/integration/testde/TestDeTest.java
+++ src/test/java/ilarkesto/integration/testde/TestDeTest.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -76,7 +76,7 @@ public class TestDeTest extends ATest {
 			log.info(html);
 
 			assertContains(html, "Druckerpatronen 03/2014");
-			assertContains(html, "gut (1,8)");
+			assertContains(html, " (1,8)");
 
 			assertContainsNot(html, "Zurück zum Artikel");
 			assertContainsNot(html, "Aktivierte Filter");
