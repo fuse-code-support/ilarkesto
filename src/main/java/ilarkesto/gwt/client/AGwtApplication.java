0238199d1f233b11ec70c98baf7332430f48c045
Witoslaw Koczewski <wi@koczewski.de>
2016-02-04 15:15:03 +0100

    gwt logging

--- src/main/java/ilarkesto/gwt/client/AGwtApplication.java
+++ src/main/java/ilarkesto/gwt/client/AGwtApplication.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -128,7 +128,7 @@ public abstract class AGwtApplication<D extends ADataTransferObject> implements 
 		}
 		if (data.containsEntities()) {
 			Collection<Map<String, String>> entityDatas = data.getEntities();
-			log.debug("entities received:", entityDatas);
+			log.debug("entities received:", entitiesDataAsString(entityDatas));
 			Set<AEntity> entities = getEntitiesBackend().updateFromServer(entityDatas);
 			onEntitiesReceived(entities);
 		}
@@ -138,6 +138,15 @@ public abstract class AGwtApplication<D extends ADataTransferObject> implements 
 			onUserIdReceived(userId);
 		}
 		onServerDataReceived(data);
+	}
+
+	private String entitiesDataAsString(Collection<Map<String, String>> entityDatas) {
+		StringBuilder sb = new StringBuilder();
+		for (Map<String, String> entity : entityDatas) {
+			if (sb.length() > 0) sb.append(", ");
+			sb.append(entity.get("@type")).append(":").append(entity.get("id"));
+		}
+		return sb.toString();
 	}
 
 	protected void onServiceCallSuccessfullyProcessed(AServiceCall<D> serviceCall) {}
