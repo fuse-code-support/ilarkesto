ad2a9c160fcdaf20b49cd4ce682dc6453888e26c
Witoslaw Koczewski <wi@koczewski.de>
2016-02-08 08:56:00 +0100

    gwt compatibility

--- src/main/java/ilarkesto/core/persistance/ATransaction.java
+++ src/main/java/ilarkesto/core/persistance/ATransaction.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -239,10 +239,10 @@ public abstract class ATransaction<E extends Entity> implements EntitiesProvider
 
 	@Override
 	public E findFirst(AEntityQuery query) {
-		E entity = modified.findFirst(query);
+		Entity entity = modified.findFirst(query);
 		if (entity == null) entity = getBackend().findFirst(query);
 		if (entity != null && deleted.contains(entity.getId())) return null;
-		return entity;
+		return (E) entity;
 	}
 
 	@Override
