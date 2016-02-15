adccb5d0260e32c8e3b87c55f99887e183e75da1
Witoslaw Koczewski <wi@koczewski.de>
2016-02-15 13:53:25 +0100

    http

--- src/main/java/ilarkesto/net/httpclient/HttpCookie.java
+++ src/main/java/ilarkesto/net/httpclient/HttpCookie.java
@@ -1,19 +1,18 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
 package ilarkesto.net.httpclient;
-
 
 public class HttpCookie {
 
@@ -24,6 +23,11 @@ public class HttpCookie {
 		int idx = s.indexOf(';');
 		if (idx > 0) s = s.substring(0, idx);
 		idx = s.indexOf('=');
+		if (idx < 0) {
+			name = "";
+			value = "";
+			return;
+		}
 		name = s.substring(0, idx).trim();
 		value = s.substring(idx + 1).trim();
 	}
--- src/main/java/ilarkesto/net/httpclient/HttpResponse.java
+++ src/main/java/ilarkesto/net/httpclient/HttpResponse.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>
- *
+ * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- *
+ * 
  * You should have received a copy of the GNU Affero General Public License along with this program. If not,
  * see <http://www.gnu.org/licenses/>.
  */
@@ -110,6 +110,7 @@ public class HttpResponse {
 		List<String> cookies = getHeaderValues(Http.RESPONSE_HEADER_SET_COOKIE);
 		if (cookies == null || cookies.isEmpty()) return;
 		for (String cookie : cookies) {
+			if (Str.isBlank(cookie)) continue;
 			request.getSession().addCookie(new HttpCookie(cookie));
 		}
 	}
