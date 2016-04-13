50f102bde1895c6060e318eec6c17fddd5460e7b
Witoslaw Koczewski <wi@koczewski.de>
2016-04-13 12:23:58 +0200

    direktory delete on mac

--- src/main/java/ilarkesto/io/IO.java
+++ src/main/java/ilarkesto/io/IO.java
@@ -827,11 +827,16 @@ public abstract class IO {
 		if (!dir.isDirectory()) return false;
 		for (File file : dir.listFiles()) {
 			if (file.getName().equals(MAC_SYS_FILENAME)) {
-				delete(file);
 				continue;
 			}
 			if (!file.isDirectory()) return false;
 			if (!deleteDirectorysIfEmpty(file)) return false;
+		}
+		for (File file : dir.listFiles()) {
+			if (file.getName().equals(MAC_SYS_FILENAME)) {
+				delete(file);
+				continue;
+			}
 		}
 		return dir.delete();
 	}
