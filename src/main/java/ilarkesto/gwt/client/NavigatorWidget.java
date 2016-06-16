10658a10dfa90b6c14396ba3914885a28f8fe5be
Witoslaw Koczewski <wi@koczewski.de>
2016-06-16 15:32:53 +0200

    gwt

--- src/main/java/ilarkesto/gwt/client/NavigatorWidget.java
+++ src/main/java/ilarkesto/gwt/client/NavigatorWidget.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2011 Witoslaw Koczewsi <wi@koczewski.de>, Artjom Kochtchi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
  * License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -29,6 +29,7 @@ import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
+@Deprecated
 public class NavigatorWidget<K extends Object> extends AWidget {
 
 	private static final Log log = Log.get(NavigatorWidget.class);
