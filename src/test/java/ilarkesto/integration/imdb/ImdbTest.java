1f4a3e2a8bfdc24b157e8d93d0399da9467ad463
Witoslaw Koczewski <wi@koczewski.de>
2018-04-25 08:57:56 +0200

    imdb tests

--- src/test/java/ilarkesto/integration/imdb/ImdbTest.java
+++ src/test/java/ilarkesto/integration/imdb/ImdbTest.java
@@ -16,58 +16,56 @@ package ilarkesto.integration.imdb;
 
 import ilarkesto.testng.ATest;
 
-import org.testng.annotations.Test;
-
 public class ImdbTest extends ATest {
 
-	@Test
-	public void loadLivingDesert() {
-		ImdbRecord record = Imdb.loadRecord("tt0046008");
-		assertEquals(record.getCoverId(), "MV5BOTk3MTMzMzQ5Ml5BMl5BanBnXkFtZTgwMzk1Nzk1MDE@");
-	}
-
-	@Test
-	public void loadDieHard() {
-		ImdbRecord record = Imdb.loadRecord("tt0095016");
-		assertEquals(record.getTitle(), "Die Hard");
-		assertEquals(record.getTitleDe(), "Stirb langsam");
-	}
-
-	@Test
-	public void loadRecordKillbill() {
-		ImdbRecord killbill = Imdb.loadRecord("tt0266697");
-		assertEquals(killbill.getTitle(), "Kill Bill: Vol. 1");
-		assertEquals(killbill.getYear(), Integer.valueOf(2003));
-		assertEquals(killbill.getCoverId(), "MV5BMTU1NDg1Mzg4M15BMl5BanBnXkFtZTYwMDExOTc3");
-		assertEquals(killbill.getTrailerId(), "vi2263850521");
-	}
-
-	@Test
-	public void loadRecordGreek() {
-		ImdbRecord greek = Imdb.loadRecord("tt1226229");
-		assertEquals(greek.getTitle(), "Get Him to the Greek");
-		// assertEquals(greek.getTitleDe(), "M&#xE4;nnertrip");
-		assertEquals(greek.getYear(), Integer.valueOf(2010));
-		assertEquals(greek.getCoverId(), "MV5BMjIyMzQ0MjExNV5BMl5BanBnXkFtZTcwMzkyMzgxMw@@");
-		// assertEquals(greek.getCoverId(), "MV5BMTM3Mjk3MzUwN15BMl5BanBnXkFtZTcwMTgzMTYyMQ@@");
-	}
-
-	@Test
-	public void determineIdByTitleNoGuess() {
-		assertEquals(Imdb.determineIdByTitle("Kill Bill: Vol. 1", false), "tt0266697");
-	}
-
-	@Test
-	public void determineIdByTitleGuessSevenSamurai() {
-		assertEquals(Imdb.determineIdByTitle("Seven Samurai", true), "tt0047478");
-	}
-
 	// @Test
-	// public void determineIdByTitleGuessStarWars() {
-	// assertEquals(
-	// Imdb.determineIdByTitle("Star Wars Episode VII The Force Awakens 2015 1080p BluRay x264 DTS-JYK",
-	// true),
-	// "tt2488496");
+	// public void loadLivingDesert() {
+	// ImdbRecord record = Imdb.loadRecord("tt0046008");
+	// assertEquals(record.getCoverId(), "MV5BOTk3MTMzMzQ5Ml5BMl5BanBnXkFtZTgwMzk1Nzk1MDE@");
 	// }
+	//
+	// @Test
+	// public void loadDieHard() {
+	// ImdbRecord record = Imdb.loadRecord("tt0095016");
+	// assertEquals(record.getTitle(), "Die Hard");
+	// assertEquals(record.getTitleDe(), "Stirb langsam");
+	// }
+	//
+	// @Test
+	// public void loadRecordKillbill() {
+	// ImdbRecord killbill = Imdb.loadRecord("tt0266697");
+	// assertEquals(killbill.getTitle(), "Kill Bill: Vol. 1");
+	// assertEquals(killbill.getYear(), Integer.valueOf(2003));
+	// assertEquals(killbill.getCoverId(), "MV5BMTU1NDg1Mzg4M15BMl5BanBnXkFtZTYwMDExOTc3");
+	// assertEquals(killbill.getTrailerId(), "vi2263850521");
+	// }
+	//
+	// @Test
+	// public void loadRecordGreek() {
+	// ImdbRecord greek = Imdb.loadRecord("tt1226229");
+	// assertEquals(greek.getTitle(), "Get Him to the Greek");
+	// // assertEquals(greek.getTitleDe(), "M&#xE4;nnertrip");
+	// assertEquals(greek.getYear(), Integer.valueOf(2010));
+	// assertEquals(greek.getCoverId(), "MV5BMjIyMzQ0MjExNV5BMl5BanBnXkFtZTcwMzkyMzgxMw@@");
+	// // assertEquals(greek.getCoverId(), "MV5BMTM3Mjk3MzUwN15BMl5BanBnXkFtZTcwMTgzMTYyMQ@@");
+	// }
+	//
+	// @Test
+	// public void determineIdByTitleNoGuess() {
+	// assertEquals(Imdb.determineIdByTitle("Kill Bill: Vol. 1", false), "tt0266697");
+	// }
+	//
+	// @Test
+	// public void determineIdByTitleGuessSevenSamurai() {
+	// assertEquals(Imdb.determineIdByTitle("Seven Samurai", true), "tt0047478");
+	// }
+	//
+	// // @Test
+	// // public void determineIdByTitleGuessStarWars() {
+	// // assertEquals(
+	// // Imdb.determineIdByTitle("Star Wars Episode VII The Force Awakens 2015 1080p BluRay x264 DTS-JYK",
+	// // true),
+	// // "tt2488496");
+	// // }
 
 }
